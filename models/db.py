ca016a4733778656b5bca11d2b7006ff04fc18e1
saitejacodes <saitejaveeramalla3@gmail.com>
2026-04-07 13:15:59 +0530

    build database connection for mongodb

---
+++ models/db.py
@@ -0,0 +1,9 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+load_dotenv()
+client=MongoClient(os.getenv("MONGO_URI"))
+db=client["cpplanner"]
+users_collection=db["users"]
+plans_collection=db["plans"]
+
