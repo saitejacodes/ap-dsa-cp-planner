8f0a95c855af3c166c33cfd1f5abc27af112957b
saitejacodes <saitejaveeramalla3@gmail.com>
2026-04-07 16:08:51 +0530

    update

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+from dotenv import load_dotenv
+import os
+load_dotenv()
 import streamlit as st
 st.set_page_config(page_title="CP Planner AI",layout="wide",initial_sidebar_state="expanded")
 if "user_id" not in st.session_state:
