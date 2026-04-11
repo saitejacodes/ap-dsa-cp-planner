fc4434dc29f67a33308607e4a2179cd302f14a77
saitejacodes <saitejaveeramalla3@gmail.com>
2026-04-11 17:38:18 +0530

    embedding-fix

--- rag/vectorstore.py
+++ rag/vectorstore.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from chromadb.utils.embedding_functions import GoogleGenerativeAiEmbeddingFunction
 
-embedding_fn=GoogleGenerativeAiEmbeddingFunction(api_key=os.environ.get("GEMINI_API_KEY"),task_type="RETRIEVAL_DOCUMENT", model_name="models/text-embedding-004")
+embedding_fn=GoogleGenerativeAiEmbeddingFunction(api_key=os.environ.get("GEMINI_API_KEY"),task_type="RETRIEVAL_DOCUMENT", model_name="models/gemini-embedding-001")
 client=chromadb.PersistentClient(path="./chroma_db")
 dsa_collection=client.get_or_create_collection(name="dsa_problems",embedding_function=embedding_fn,metadata={"hnsw:space":"cosine"})
 math_collection=client.get_or_create_collection(name="math_prereqs",embedding_function=embedding_fn)
